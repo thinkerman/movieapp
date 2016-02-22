10a639dd42d2e3f656d0a18d3c863b2e20b8d19f
Thinkerman <snipps0233@gmail.com>
2016-02-22 13:32:29 +0200

    Initial Commit

---
+++ MovieGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieGuide
+//
+//  Created by Samuel on 22/02/2016.
+//  Copyright © 2016 Veedoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
